33b0965ffb6b122d8b3f2d26a0942e2771f2c647
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-14 11:44:15 +0530

    imports removed

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,12 +3,6 @@ import { Link } from "react-router-dom";
 import JSONDATA from "./../Data/Portfolio.json";
 import ProWork from "./ProWork";
 import PCss from "./CSS/Portfolio.module.css";
-import img0 from "../img/dance1.png";
-import img1 from "../img/ecc.png";
-import img2 from "../img/mern.png";
-import img3 from "../img/api.png";
-import img4 from "../img/CustomVid.png";
-import img5 from "../img/memorygame.png";
 
 export default function Portfolio() {
   return (
