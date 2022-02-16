4ec54c0723c8ef596f3d0cd3ee432ae7f2ef950d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-17 01:51:47 +0530

    pdf loaded

--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -1,26 +1,43 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import React, { Component, useState } from "react";
 import Nav from "./../Components/Error/Nav";
-// import CvPdf from "./../pdf/Cv.pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+import CvPdf from "./../pdf/Cv.pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default function Cv() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+class Cv extends Component {
+  state = { numPages: null, pageNumber: 1 };
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-  return (
-    <div>
-      <Nav />
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  goToPrevPage = () =>
+    this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
+  goToNextPage = () =>
+    this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    return (
       <div>
-        <Document file="./Cv.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
-        </Document>
+        <nav>
+          <button onClick={this.goToPrevPage}>Prev</button>
+          <button onClick={this.goToNextPage}>Next</button>
+        </nav>
+
+        <div style={{ width: 600 }}>
+          <Document file={CvPdf} onLoadSuccess={this.onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} width={600} />
+          </Document>
+        </div>
+
         <p>
           Page {pageNumber} of {numPages}
         </p>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Cv;
