16611508f02ece01bb58f119e9d2d1af84d0eff7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-17 01:32:18 +0530

    pdf added

--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Nav from "./../Components/Error/Nav";
+import CvPdf from "./../pdf/Cv.pdf";
 
 export default function Cv() {
-  return <div>Cv</div>;
+  return (
+    <div>
+      <Nav />
+      <div>
+        <embed src={CvPdf} type="application/pdf" />
+      </div>
+    </div>
+  );
 }
