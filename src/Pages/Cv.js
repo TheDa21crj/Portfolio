c9b18f068b82b4ddb0b47ed426aef8a2d996c728
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-17 01:46:39 +0530

    working on adding pdf

--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import Nav from "./../Components/Error/Nav";
-import CvPdf from "./../pdf/Cv.pdf";
+// import CvPdf from "./../pdf/Cv.pdf";
 
 export default function Cv() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
   return (
     <div>
       <Nav />
       <div>
-        <embed src={CvPdf} type="application/pdf" />
+        <Document file="./Cv.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
       </div>
     </div>
   );
