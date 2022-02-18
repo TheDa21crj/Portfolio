f70f60fb2aaa574bfa7cc53edf538e967fe889c9
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-18 12:37:36 +0530

    recreating Cv page

--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -1,53 +1,10 @@
-import React, { Component, useState } from "react";
+import React from "react";
 import Nav from "./../Components/Error/Nav";
-import CvCss from "./CSS/Cv.module.css";
-import { Document, Page, pdfjs } from "react-pdf";
-import CvPdf from "./../pdf/Cv.pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-class Cv extends Component {
-  state = { numPages: null, pageNumber: 1 };
-
-  onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  };
-
-  goToPrevPage = () =>
-    this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
-  goToNextPage = () =>
-    this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
-
-  render() {
-    const { pageNumber, numPages } = this.state;
-
-    return (
-      <div className={CvCss.CvCdiv}>
-        <Nav />
-        <div>
-          <div className={CvCss.prevNextDiv}>
-            <button onClick={this.goToPrevPage} className={CvCss.prev}>
-              Prev
-            </button>
-            <button onClick={this.goToNextPage} className={CvCss.next}>
-              Next
-            </button>
-          </div>
-
-          <div className={CvCss.CvmDiv}>
-            <div>
-              <Document file={CvPdf} onLoadSuccess={this.onDocumentLoadSuccess}>
-                <Page pageNumber={pageNumber} width={600} />
-              </Document>
-            </div>
-          </div>
-
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-        </div>
-      </div>
-    );
-  }
+export default function Cv() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
-
-export default Cv;
