379f615effc7c66537d7b4a591bfaba218688068
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-18 15:07:13 +0530

    removing react-pdf

--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -1,67 +1,49 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import Nav from "./../Components/Error/Nav";
 import CvCss from "./CSS/Cv.module.css";
-import { Document, Page, pdfjs } from "react-pdf";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import CvPdf from "./../pdf/Cv.pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import AttachmentIcon from "@material-ui/icons/Attachment";
 
-class Cv extends Component {
-  state = { numPages: null, pageNumber: 1 };
-
-  onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  };
-
-  //   goToPrevPage = () =>
-  //     this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
-  //   goToNextPage = () =>
-  //     this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
-  render() {
-    const { pageNumber, numPages } = this.state;
-
-    return (
-      <div className={CvCss.CvCmDiv}>
-        <Nav />
-        <div className={CvCss.CvmDiv}>
-          <div className={CvCss.CvBtnDiv}>
-            <div className={CvCss.CvBtnDivLeft}>
-              <p>Cv.pdf</p>
-            </div>
-            <div className={CvCss.CvBtnDivMiddle}>
-              <button
-                //   onClick={this.goToPrevPage}
-                className={CvCss.prev}
+export default function Cv() {
+  return (
+    <div className={CvCss.CvCmDiv}>
+      <Nav />
+      <div className={CvCss.CvmDiv}>
+        <div className={CvCss.CvBtnDiv}>
+          <div className={CvCss.CvBtnDivLeft}>
+            <p>Cv.pdf</p>
+          </div>
+          <div className={CvCss.CvBtnDivMiddle}>
+            <button className={CvCss.prev}>Prev</button>
+            <button className={CvCss.next}>Next</button>
+          </div>
+          <div className={CvCss.CvBtnDivRight}>
+            <a href={CvPdf} download>
+              <GetAppIcon fontSize="large" className={CvCss.downloadIcon} />
+            </a>
+            <div>
+              <a
+                href="https://drive.google.com/file/d/1Thms9xiChIHvwO2qIDlVYkGU1rQz2__6/view?usp=sharing"
+                target="_blank"
+                id={CvCss.icon}
               >
-                Prev
-              </button>
-              <button
-                //   onClick={this.goToNextPage}
-                className={CvCss.next}
-              >
-                Next
-              </button>
-            </div>
-            <div className={CvCss.CvBtnDivRight}>
-              <a href={CvPdf} download>
-                <GetAppIcon fontSize="large" className={CvCss.downloadIcon} />
+                <AttachmentIcon
+                  fontSize="large"
+                  className={CvCss.downloadIcon}
+                />
               </a>
+              <p className={CvCss.pTag}>Open in Google Drive</p>
             </div>
-          </div>
-          <div>
-            <Document file={CvPdf} onLoadSuccess={this.onDocumentLoadSuccess}>
-              <Page pageNumber={pageNumber} width={800} />
-            </Document>
-          </div>
-          <div className={CvCss.pageDiv}>
-            <p>
-              Page {pageNumber} of {numPages}
-            </p>
           </div>
         </div>
+        <div>
+          <embed src={CvPdf} type="application/pdf" />
+        </div>
+        <div className={CvCss.pageDiv}>
+          <p>Page 1 of 1</p>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Cv;
