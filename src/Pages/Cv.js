2d79381be4687e6f2c6b0ea79f5f05c6c5c09a6b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-18 13:29:55 +0530

    Update Cv.js

--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -1,5 +1,4 @@
 import React, { Component, useState } from "react";
-import { Link } from "react-router-dom";
 import Nav from "./../Components/Error/Nav";
 import CvCss from "./CSS/Cv.module.css";
 import { Document, Page, pdfjs } from "react-pdf";
