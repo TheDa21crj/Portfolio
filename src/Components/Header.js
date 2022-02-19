c4394a4afc9209c8372cfa4e455aa13b3c1c8aed
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-19 22:32:52 +0530

    typewriter removed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,8 @@
 import React, { useEffect } from "react";
 import HCss from "./CSS/Header.module.css";
 import "./CSS/Header.css";
-import Typewriter from "typewriter-effect";
 
 export default function Header() {
-  const [state] = useEffect({
-    title: "A Full Stack Developer",
-  });
   return (
     <div className={HCss.headmDiv} id="head">
       <div className="textDiv">
@@ -14,23 +10,7 @@ export default function Header() {
       </div>
       <div className="textDiv">
         <p className="text" id="textCursor">
-          {/* A Full Stack Developer */}
-          {state.title}
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .typeString("Hello World!")
-                .callFunction(() => {
-                  console.log("String typed out!");
-                })
-                .pauseFor(2500)
-                .deleteAll()
-                .callFunction(() => {
-                  console.log("All strings were deleted");
-                })
-                .start();
-            }}
-          />
+          A Full Stack Developer
         </p>
       </div>
     </div>
