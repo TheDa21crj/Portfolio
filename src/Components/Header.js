3998c4a52462731f5837a8726442b900abef4b9e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-19 22:29:39 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import HCss from "./CSS/Header.module.css";
 import "./CSS/Header.css";
+import Typewriter from "typewriter-effect";
 
 export default function Header() {
+  const [state] = useEffect({
+    title: "A Full Stack Developer",
+  });
   return (
     <div className={HCss.headmDiv} id="head">
       <div className="textDiv">
@@ -10,7 +14,23 @@ export default function Header() {
       </div>
       <div className="textDiv">
         <p className="text" id="textCursor">
-          A Full Stack Developer
+          {/* A Full Stack Developer */}
+          {state.title}
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Hello World!")
+                .callFunction(() => {
+                  console.log("String typed out!");
+                })
+                .pauseFor(2500)
+                .deleteAll()
+                .callFunction(() => {
+                  console.log("All strings were deleted");
+                })
+                .start();
+            }}
+          />
         </p>
       </div>
     </div>
