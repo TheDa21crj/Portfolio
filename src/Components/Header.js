46e3cbbafb2abdadfcc7672d4fe6ac1315e93e4e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-20 04:05:32 +0530

    cleaned up

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import HCss from "./CSS/Header.module.css";
 import Typewriter from "typewriter-effect";
 import video from "./../Videos/video.mp4";
