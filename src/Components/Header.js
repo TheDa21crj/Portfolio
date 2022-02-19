366fbb955cad6fb32c8295f0d95cca771d92bef4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-20 04:04:37 +0530

    overlay text working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,34 @@
 import React, { useEffect } from "react";
 import HCss from "./CSS/Header.module.css";
 import Typewriter from "typewriter-effect";
+import video from "./../Videos/video.mp4";
 import "./CSS/Header.css";
 
 export default function Header() {
   return (
     <div className={HCss.headmDiv} id="head">
-      <div className="textDiv">
-        <p className="text">I'm Rishav Raj Singh</p>
-      </div>
-      <div className="textDiv">
-        <p className="text" id="textCursor">
-          <Typewriter
-            options={{
-              strings: ["A Full Stack Developer"],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </p>
+      <video
+        src={video}
+        className={HCss.videoTag}
+        loop="true"
+        autoplay="autoplay"
+        muted
+      ></video>
+      <div className={HCss.overlay}>
+        <div className="textDiv">
+          <p className="text">I'm Rishav Raj Singh</p>
+        </div>
+        <div className="textDiv">
+          <p className="text" id="textCursor">
+            <Typewriter
+              options={{
+                strings: ["A Full Stack Developer"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </p>
+        </div>
       </div>
     </div>
   );
