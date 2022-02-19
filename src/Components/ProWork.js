cbc5808d8f3a65012ee0e6ff46c7ba8ec6c8623f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-20 00:44:12 +0530

    aos added on projects cards

--- src/Components/ProWork.js
+++ src/Components/ProWork.js
@@ -4,7 +4,7 @@ import PwCss from "./CSS/ProWork.module.css";
 
 export default function ProWork(props) {
   return (
-    <div className={PwCss.mdiv}>
+    <div className={PwCss.mdiv} data-aos="fade">
       <img src={props.img} alt="" className={PwCss.imgPw} />
       <h3 className={PwCss.pwH3}>{props.title}</h3>
       <p>{props.des}</p>
