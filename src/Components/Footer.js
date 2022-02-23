99e28e05cf032deb977638484412e3b328bebf0d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-23 20:50:41 +0530

    footer text darkmode added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,5 +2,9 @@ import React from "react";
 import FCss from "./CSS/Footer.module.css";
 
 export default function Footer() {
-  return <div className={FCss.mDiv}>Made With ❤️</div>;
+  return (
+    <div className={FCss.mDiv}>
+      <p>Made With ❤️</p>
+    </div>
+  );
 }
