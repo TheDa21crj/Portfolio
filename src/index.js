04dd05529357acf5a0faa19e5e4bce8212fddd2f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-23 15:31:46 +0530

    Provider added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
