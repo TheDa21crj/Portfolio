f6904c37334fe851111e40dd91530b3e710cc4ea
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-23 20:51:01 +0530

    useSelector

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,10 @@
 import React from "react";
 import FCss from "./CSS/Footer.module.css";
+import { useSelector } from "react-redux";
 
 export default function Footer() {
+  const myState = useSelector((state) => state.darkmode);
+
   return (
     <div className={FCss.mDiv}>
       <p>Made With ❤️</p>
