a2acac950c9ad477a0cd9f884ba19eefbca453db
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-23 21:15:25 +0530

    bug fixed

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -25,10 +25,12 @@ import { useSelector } from "react-redux";
 export default function () {
   const myState = useSelector((state) => state.darkmode);
 
+  let darkgmap_canvas =
+    myState === true ? { filter: "grayscale(20%) invert(95%)" } : {};
   return (
     <div>
       <div className="mapouter">
-        <div className={myState === true ? "darkgmap_canvas" : "gmap_canvas"}>
+        <div className="gmap_canvas" style={darkgmap_canvas}>
           <iframe
             id="gmap_canvas"
             src="https://maps.google.com/maps?q=sona%20village&t=&z=13&ie=UTF8&iwloc=&output=embed"
