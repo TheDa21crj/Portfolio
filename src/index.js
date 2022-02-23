ac0e7037dba3aad2c1eb6f6efc2a4c89f1f135f8
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-23 15:45:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
+import store from "./store";
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 ReactDOM.render(
   <BrowserRouter>
