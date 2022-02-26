d3e88b981043adfcbc3cb36e9f0a987de9fdb00a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-27 03:17:36 +0530

    Update Map.js

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,23 +1,3 @@
-// import React from "react";
-// import { Map, GoogleApiWrapper } from "google-maps-react";
-
-// export class MapContainer extends React.Component {
-//   render() {
-//     return (
-//       <Map
-//         google={this.props.google}
-//         style={{ width: "100%", height: "100%" }}
-//         initialCenter={{ lat: 27.2046, lng: 77.4977 }}
-//         zoom={15}
-//       />
-//     );
-//   }
-// }
-
-// export default GoogleApiWrapper({
-//   apiKey: "AIzaSyCoGEg5Bgw7PSWLQpbGkDUs3LlvOAAO6zE",
-// })(MapContainer);
-
 import React from "react";
 import "./CSS/Map.css";
 import { useSelector } from "react-redux";
