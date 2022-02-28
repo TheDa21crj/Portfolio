587a88f6e842f16cb36e48cd81a64a08778d500a
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-28 22:54:11 +0100

    Update ProWork.js

--- src/Components/ProWork.js
+++ src/Components/ProWork.js
@@ -9,14 +9,9 @@ export default function ProWork(props) {
       <img src={props.img} alt="" className={PwCss.imgPw} />
       <h3 className={PwCss.pwH3}>{props.title}</h3>
       <p>{props.des}</p>
-      {/* <div> */}
       <Link to={props.link} target="_blank" className={PwCss.LinkP}>
         Github
       </Link>
-      {/* <Link to={props.link} target="_blank" className={PwCss.LinkP}>
-          Info
-        </Link> */}
-      {/* </div> */}
     </div>
   );
 }
