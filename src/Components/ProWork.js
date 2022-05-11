1c457827ee2b307a27f0fe2242fee99eafb6fa09
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-11 08:57:59 +0530

    Update ProWork.js

--- src/Components/ProWork.js
+++ src/Components/ProWork.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PwCss from "./CSS/ProWork.module.css";
 import InfoIcon from "@material-ui/icons/Info";
