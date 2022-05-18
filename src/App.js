0e9d947ecc2878eee97cde93705fdf8ebd8d1e45
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-18 11:35:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { Route, Switch } from "react-router-dom";
-import Layout from "./Pages/Layout";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Layout>
-        {/* <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/cv" exact>
-            <Cv />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch> */}
-        hello
-      </Layout>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
