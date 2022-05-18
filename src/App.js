c9da71a0e416fb44ff9719b8601c89ac6b6bcaa7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-18 11:41:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Pages/Layout";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        Hello World
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes> */}
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
