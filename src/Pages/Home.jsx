e0335e0378f8a394cbcf2729d1c0dd8da2af1536
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-18 12:10:20 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Background from "./../Components/Background";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Background />
+    </div>
+  );
 }
