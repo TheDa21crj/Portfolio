d9d7bcecdcab92d6e03a37c9d65a7fe47f14f332
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-18 21:42:16 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "./../Components/Nav";
+import Header from "./../Components/Header";
+import MyBio from "../Components/MyBio";
 
 export default function Home() {
   return (
     <div>
       <Nav />
+      <Header />
+      <MyBio />
     </div>
   );
 }
