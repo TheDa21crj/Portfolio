7c09d3325aabb6f2dff2b3c45dfb4da10ebc50fa
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-18 12:00:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        Hello World
-        {/* <Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Error />} />
-        </Routes> */}
+        </Routes>
       </Layout>
     </BrowserRouter>
   );
