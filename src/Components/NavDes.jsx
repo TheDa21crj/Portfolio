71a643f26ad119f93cd6a99c0ce6647f550b40da
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-18 21:31:24 +0530

    Update NavDes.jsx

--- src/Components/NavDes.jsx
+++ src/Components/NavDes.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import NavUl from "./NavUl";
 import NDCss from "./Css/NavDes.module.css";
 
 export default function NavDes() {
-  return <div>NavDes</div>;
+  return (
+    <div className={NDCss.mDiv}>
+      <div className={NDCss.NavmDiv}>
+        <ul className={NDCss.ul}>
+          <NavUl liName="Home" />
+          <NavUl liName="About" />
+          <NavUl liName="Portfolio" />
+          <NavUl liName="Services" />
+          <NavUl liName="Contact" />
+        </ul>
+      </div>
+    </div>
+  );
 }
