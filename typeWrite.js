83f0e58a564063b39d03733a7b648e693ad4547c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-19 16:35:12 +0530

    Update typeWrite.js

--- typeWrite.js
+++ typeWrite.js
@@ -1,5 +1,6 @@
 new Typewriter(document.getElementById("typewriter"), {
-    strings: ["A Full Stack Developer", "UI/UX Developer"],
+    // strings: ["A Full Stack Developer", "UI/UX Developer"],
+    strings: ["A Full Stack Developer"],
     autoStart: true,
     loop: true,
 });
