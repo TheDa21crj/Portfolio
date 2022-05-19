a695d45abad3efff30d78f7010f5fe5edf750e81
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-19 16:17:42 +0530

    Update typeWrite.js

--- typeWrite.js
+++ typeWrite.js
@@ -1,13 +1,5 @@
-// import Typewriter from "typewriter-effect/dist/core";
-
 new Typewriter(document.getElementById("typewriter"), {
     strings: ["A Full Stack Developer", "UI/UX Developer"],
     autoStart: true,
     loop: true,
 });
-
-// options={{
-//     strings: ["A Full Stack Developer"],
-//     autoStart: true,
-//     loop: true,
-//   }}
