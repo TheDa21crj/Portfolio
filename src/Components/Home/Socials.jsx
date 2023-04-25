37e60a813dca54dd15ca97109260ee44cbb54e7c
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-25 19:54:28 +0530

    Update Socials.jsx

--- src/Components/Home/Socials.jsx
+++ src/Components/Home/Socials.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 // css
 import SoCss from "./Css/Socials.module.css";
 
@@ -10,12 +10,16 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 export default function Socials() {
   return (
     <div className={SoCss.mDiv}>
-      <LinkedInIcon
-        fontSize="large"
-        style={{ color: "#0a66c2" }}
-        className={SoCss.LinkedIn}
-      />
-      <GitHubIcon fontSize="large" className={SoCss.Github} />
+      <a href="https://www.linkedin.com/in/rishav-singh-a89622211/">
+        <LinkedInIcon
+          fontSize="large"
+          style={{ color: "#0a66c2" }}
+          className={SoCss.LinkedIn}
+        />
+      </a>
+      <a href="https://github.com/TheDa21crj">
+        <GitHubIcon fontSize="large" className={SoCss.Github} />
+      </a>
     </div>
   );
 }
