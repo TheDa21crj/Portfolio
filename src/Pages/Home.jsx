f9fa8c2f480715ad00e547873565e6b511af68db
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-03 22:20:30 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 // Components
 import Bio from "./../Components/Home/Bio";
 import Header from "./../Components/Home/Header";
-import Contact from "./../Components/Home/Contact";
+import Contact from "./../Componen ts/Home/Contact";
 import Services from "./../Components/Home/Services";
 import Portfolio from "./../Components/Home/Portfolio";
 
