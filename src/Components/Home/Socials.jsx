ae3dec84a1af47df6fdfb0499e73d84e0e8baf4d
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-03 22:02:15 +0530

    Update Socials.jsx

--- src/Components/Home/Socials.jsx
+++ src/Components/Home/Socials.jsx
@@ -10,10 +10,7 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 export default function Socials() {
   return (
     <div className={SoCss.mDiv}>
-      <a
-        href="https://www.linkedin.com/in/rishav-singh-a89622211/"
-        target="_blank"
-      >
+      <a href="https://www.linkedin.com/in/rishav--singh/" target="_blank">
         <LinkedInIcon fontSize="large" className={SoCss.LinkedIn} />
       </a>
       <a href="https://github.com/TheDa21crj" target="_blank">
