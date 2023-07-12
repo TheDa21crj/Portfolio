ab7233ba7ddc59322c5f8b450cbcbbda4fbc0667
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-12 20:27:00 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // Components
 import Bio from "./../Components/Home/Bio";
@@ -6,15 +6,23 @@ import Header from "./../Components/Home/Header";
 import Contact from "./../Components/Home/Contact";
 import Services from "./../Components/Home/Services";
 import Portfolio from "./../Components/Home/Portfolio";
+import Nav from "./../Components/Nav";
 
 export default function Home() {
+  // scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
+      <Nav leftTitle="My Portfolio" />
       <Header />
       <Bio />
       <Portfolio />
       <Services />
-      <Contact />
+      <section id="ContactUS">
+        <Contact />
+      </section>
     </div>
   );
 }
