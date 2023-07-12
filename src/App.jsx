093846dba251040b676ec13e7bf6d54f0e33eb85
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-12 20:26:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,10 @@ import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
 
-// Components
-import Nav from "./Components/Nav";
-
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        <Nav leftTitle="My Portfolio" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Error />} />
