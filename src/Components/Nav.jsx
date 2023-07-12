2b694184c7539ea368d5747ce483842a90dbb247
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-12 20:00:01 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -14,18 +14,9 @@ export default function Nav(props) {
           setToggle(!toggle);
         }}
       >
-        <div
-          className={NCss.bur1}
-          id={toggle ? "toogleTrueBur1" : "toogleFalseBur1"}
-        ></div>
-        <div
-          className={NCss.bur2}
-          id={toggle ? "toogleTrueBur2" : "toogleFalseBur2"}
-        ></div>
-        <div
-          className={NCss.bur3}
-          id={toggle ? "toogleTrueBur3" : "toogleFalseBur3"}
-        ></div>
+        <div className={NCss.bur1}></div>
+        <div className={NCss.bur2}></div>
+        <div className={NCss.bur3}></div>
       </div>
     </div>
   );
