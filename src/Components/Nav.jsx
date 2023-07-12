3facc55e7e7fdc6e99f5cb68746e8b756dbc13bb
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-12 20:26:50 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,23 +1,66 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 // css
 import NCss from "./Css/Nav.module.css";
 
 export default function Nav(props) {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className={NCss.mDiv}>
-      <p className={NCss.pTagHead}>{props.leftTitle}</p>
-      <div
-        className={NCss.hamburger}
-        onClick={() => {
-          setToggle(!toggle);
-        }}
-      >
-        <div className={NCss.bur1}></div>
-        <div className={NCss.bur2}></div>
-        <div className={NCss.bur3}></div>
+    <>
+      <div className={NCss.mDiv}>
+        <p className={NCss.pTagHead}>
+          <Link to="/" className="LinkStyle">
+            {props.leftTitle}
+          </Link>
+        </p>
+
+        {/* <div
+          className={NCss.hamburger}
+          onClick={() => {
+            setToggle(!toggle);
+          }}
+        >
+          <div className={NCss.bur1}></div>
+          <div className={NCss.bur2}></div>
+          <div className={NCss.bur3}></div>
+        </div> */}
       </div>
-    </div>
+      <div className={NCss.moboNav} id={toggle ? "showNav" : "HideNav"}>
+        <p className={NCss.mobComponent}>
+          <Link
+            to="/#Header"
+            className="LinkStyle"
+            onClick={() => {
+              setToggle(false);
+            }}
+          >
+            Home
+          </Link>
+        </p>
+        <p className={NCss.mobComponent}>
+          <Link
+            to="/#Services"
+            className="LinkStyle"
+            onClick={() => {
+              setToggle(false);
+            }}
+          >
+            Services
+          </Link>
+        </p>
+        <p className={NCss.mobComponent}>
+          <Link
+            to="/#Contact"
+            className="LinkStyle"
+            onClick={() => {
+              setToggle(false);
+            }}
+          >
+            Contact
+          </Link>
+        </p>
+      </div>
+    </>
   );
 }
